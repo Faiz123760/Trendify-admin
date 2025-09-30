3dafc82c0b3dd149472dbc18442f22f96a7f040c
FaizMullyPra <faiz47532@gmail.com>
2025-09-30 11:11:33 +0530

    feat: add admin project files

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { backendUrl, currency } from "../App";
+import { toast } from "react-toastify";
+import { assets } from "../assets/assets";
+
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/list",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setOrders(response.data.orders.reverse());
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: event.target.value },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
+  return (
+    <div className="p-4 md:p-8">
+      <h3 className="text-2xl font-bold mb-6">Your Orders</h3>
+      <div>
+        {orders.length > 0 ? (
+          orders.map((order) => (
+            <div
+              className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 rounded-lg"
+              key={order._id}
+            >
+              <img
+                className="w-12"
+                src={assets.parcel_icon}
+                alt="Parcel Icon"
+              />
+              <div>
+                <div>
+                  {order.items.map((item, itemIndex) => (
+                    <p className="py-0.5" key={itemIndex}>
+                      {item.name} X {item.quantity} <span>{item.size}</span>
+                      {itemIndex !== order.items.length - 1 && ","}
+                    </p>
+                  ))}
+                </div>
+                <p className="mt-3 mb-2 font-medium">
+                  {order.address.firstName + " " + order.address.lastName}
+                </p>
+                <div>
+                  <p>{order.address.street + ", "}</p>
+                  <p>
+                    {order.address.city +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.country +
+                      ", " +
+                      order.address.zipcode}
+                  </p>
+                </div>
+                <p>{order.address.phone}</p>
+              </div>
+
+              <div>
+                <p className="text-sm sm:text-[15px]">
+                  Items: {order.items.length}
+                </p>
+                <p className="mt-3">Method: {order.paymentMethod}</p>
+                <p>Payment: {order.payment ? "Done" : "Pending"}</p>
+                <p>Date: {new Date(order.date).toLocaleDateString()}</p>
+              </div>
+              <p className="text-sm sm:text-[15px]">
+                {currency}
+                {order.amount}
+              </p>
+              <select
+                onChange={(event) => statusHandler(event, order._id)}
+                value={order.status}
+                className="p-2 font-semibold border rounded-md"
+              >
+                <option value="Order Placed">Order Placed</option>
+                <option value="Packing">Packing</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Out For delivery">Out For delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          ))
+        ) : (
+          <div className="text-center py-8">
+            <p className="text-gray-500">No orders found</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
